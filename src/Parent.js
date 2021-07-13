e1dfdfe626eca1fecd73372412fbd7a4fa4e5b94
AnushahNadir <anushahnadir@gmail.com>
2021-07-14 02:23:52 +0500

    update state eith usecontext

---
+++ src/Parent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Child from './Child.js'
+function Parent() {
+  return (
+    <div>
+      Parent
+      <Child></Child>
+    </div>
+  );
+}
+
+export default Parent;
